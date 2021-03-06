283d0b44cd6387fc2255b4a04621bc24477ab2b3
SgtGigdi <66046963+SgtGigdi@users.noreply.github.com>
2021-03-06 12:11:13 -0600

    test

--- src/postinstall.js
+++
@@ -1,8 +0,0 @@
-var extract = require("extract-zip");
-extract(
-    `${__dirname}/assets/pack.zip`,
-    { dir: `${__dirname}/assets/pack` },
-    function (err) {
-        console.log(err);
-    }
-);
